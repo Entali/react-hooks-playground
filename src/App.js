fc28598c59e27007dbc1a8de9193ce8766a9a59f
Nata Entali <nata.entali@protonmail.com>
2021-02-05 18:10:37 +0200

    Playground component

--- src/App.js
+++ src/App.js
@@ -5,10 +5,42 @@ function App() {
   return (
       <div className="App">
         <header className="App-header">
-          Playground
+          <Playground/>
         </header>
       </div>
   );
 }
+
+const Playground = () => {
+  const [state, setState] = useState({
+    text: '',
+    checked: false
+  })
+
+  const mergeState = partialState => setState((prevState => ({
+    ...prevState,
+    ...partialState
+  })));
+
+  const onTextChange = e => mergeState({ text: e.target.value});
+  const onCheckboxChange = () => mergeState({
+    checked: !state.checked
+  });
+
+  return (
+      <div style={{
+        width: '300px',
+        display: 'flex',
+        flexDirection: 'column'
+      }}>
+        <input type="text" onChange={onTextChange}/>
+        <input type="checkbox" onChange={onCheckboxChange}/>
+        <input type="text" value={state.text} readOnly/>
+        <input type="text" value={state.checked.toString()} readOnly/>
+      </div>
+  )
+}
+
+
 
 export default App;
