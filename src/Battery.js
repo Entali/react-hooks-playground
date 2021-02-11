b84885980051a4e94833458f682a72729b85bff8
Nata Entali <nata.entali@protonmail.com>
2021-02-11 10:42:38 +0200

    Battery component

---
+++ src/Battery.js
@@ -0,0 +1,35 @@
+import React, {useState, useEffect} from 'react';
+
+const Battery = () => {
+  const [battery, setBattery] = useState({
+    level: 0,
+    charging: false
+  });
+
+  const onChange = ({ target: { level, charging } }) => {
+    setBattery({ level, charging });
+  };
+
+  useEffect(() => {
+    let battery;
+    navigator.getBattery().then(bat => {
+      battery = bat;
+      battery.addEventListener('levelchange', onChange);
+      battery.addEventListener('chargingchange', onChange);
+      onChange({ target: battery });
+    })
+
+    return () => {
+      battery.removeEventListener('levelchange', onChange);
+      battery.removeEventListener('chargingchange', onChange);
+    }
+  }, []);
+
+  return (
+      <div>
+        Battery
+      </div>
+  );
+};
+
+export default Battery;
